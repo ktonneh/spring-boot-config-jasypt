e835890b9e6f384ddaba93c1975c0a5e53055522
thexpertdev <thexpertdev@li-2.local>
2024-08-11 09:18:18 +0300

    initial commit

---
+++ src/main/java/com/thexpertdev/jasyptdemo/SpringBootConfigJasyptApplication.java
@@ -0,0 +1,13 @@
+package com.thexpertdev.jasyptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigJasyptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigJasyptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thexpertdev/jasyptdemo/SpringBootConfigJasyptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thexpertdev.jasyptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigJasyptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
